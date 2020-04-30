d560c7317c16e12c6fbebecf415e315ace85efb4
15082037343@163.com <15082037343@163.com>
2020-04-30 20:55:10 +0800

    start-all.bat

--- platform-assembly/shell/start-all.bat
+++ platform-assembly/shell/start-all.bat
@@ -1,7 +1,9 @@
-
 @echo off
-
-
+start java -jar lib\\platform-eureka.jar
+start java -jar lib\\platform-provider-user.jar
+start java -jar lib\\platform-client-user.jar
+start java -jar lib\\platform-dashboard.jar
+start java -jar lib\\platform-proxy.jar
 exit
 
 
