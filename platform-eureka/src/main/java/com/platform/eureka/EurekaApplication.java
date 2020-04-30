0ba82ffc018337ac534c110647ffe252dcc51321
admin <15082037343@163.com>
2020-04-30 17:33:51 +0800

    security

--- platform-eureka/src/main/java/com/platform/eureka/EurekaApplication.java
+++ platform-eureka/src/main/java/com/platform/eureka/EurekaApplication.java
@@ -3,13 +3,25 @@ package com.platform.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 
 @EnableEurekaServer
 @SpringBootApplication
-public class EurekaApplication {
+@EnableWebSecurity
+public class EurekaApplication extends WebSecurityConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class, args);
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        super.configure(http);
+    }
+
 }
+
