3301fb65371c5b9e4a552d1d95410e690dc5133e
15082037343@163.com <15082037343@163.com>
2020-06-16 20:06:39 +0800

    provider

---
+++ platform-provider/src/main/java/com/platform/provider/ProviderApplication.java
@@ -0,0 +1,19 @@
+package com.platform.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+@EnableDiscoveryClient
+@EnableSwagger2
+@SpringBootApplication(scanBasePackages = { "com.platform.module.entity", "com.platform.provider" })
+public class ProviderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderApplication.class, args);
+    }
+
+}
+
